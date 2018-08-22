a0a39cdafaaeac53b8132096d02c37b764bdc23a
ALUMNO-E <ALUMNO-E@UCSP.AULAB>
2018-08-22 09:00:34 -0500

    cambio

--- Proyecto1/main.cpp
+++ Proyecto1/main.cpp
@@ -4,6 +4,11 @@ using namespace std;
 
 int main()
 {
+    int a,b,z=0;
+    a = 5;
+    b = 6;
+    z = a + b;
     cout << "Hello Hector! , como andas? :v" << endl;
+    cout << "El resultado es "<<z<<endl;
     return 0;
 }
