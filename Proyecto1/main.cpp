56a2a55c651410d6d5a2c9992697853151f6018a
ALUMNO-E <ALUMNO-E@UCSP.AULAB>
2018-08-22 09:15:30 -0500

    hola

--- Proyecto1/main.cpp
+++ Proyecto1/main.cpp
@@ -5,10 +5,8 @@ using namespace std;
 int main()
 {
     int a,b,z=0;
-    a = 5;
-    b = 6;
+    cout<<"Digite un valor para a y b: "; cin>>a>>b;
     z = a + b;
-    cout << "Hello Hector! , como andas? :v" << endl;
-    cout << "El resultado es "<<z<<endl;
+    cout << "La suma de "<<a<<" y "<<b<<" es igual a "<< z<<endl;
     return 0;
 }
